116ff9a4f59a2882e275ce61aad0e9c2919fe14e
SG60 <samjg60@gmail.com>
2015-09-17 22:45:11 +0100

    Finish D3DClass::Initialize function

--- D3DEngine/D3DClass.h
+++ D3DEngine/D3DClass.h
@@ -1,19 +1,11 @@
 #pragma once
 
 /////////////////////
-// LINKING
-//
-#pragma comment(lib, "dxgi.lib")
-#pragma comment(lib, "d3d11.lib")
-
-/////////////////////
 // INCLUDES
 //
 #include <dxgi.h>
-#include <d3dcommon.h>
 #include <d3d11.h>
 #include <DirectXMath.h>
-#include <DirectXPackedVector.h>
 
 //////////////////////////////
 // D3DClass
