fa3213198ca116f9ee96dfeee21d2665ce43b57d
SG60 <samjg60@gmail.com>
2015-12-08 21:52:09 +0000

    Finish ModelClass
    RenderBuffers()

--- D3DEngine/ModelClass.cpp
+++ D3DEngine/ModelClass.cpp
@@ -141,3 +141,47 @@ bool ModelClass::InitializeBuffers(ID3D11Device* device)
 
 	return true;
 }
+
+// Release the vertex and index buffers that were created in InitializeBuffers.
+void ModelClass::ShutdownBuffers()
+{
+	// Release the index buffer.
+	if (m_indexBuffer)
+	{
+		m_indexBuffer->Release();
+		m_indexBuffer = nullptr;
+	}
+
+	// Release the vertex buffer.
+	if(m_vertex_buffer)
+	{
+		m_vertex_buffer->Release();
+		m_vertex_buffer = nullptr;
+	}
+
+	return;
+}
+
+// Set the vertex buffer and index buffer as active on the input assembler in the GPU. Once
+// the GPU has an active vertex buffer it can then use the shader to render that buffer. This
+// function also defines how those buffers should drawn (such as triangles, lines etc).
+void ModelClass::RenderBuffers(ID3D11DeviceContext* deviceContext)
+{
+	unsigned int stride;
+	unsigned int offset;
+
+	// Set vertex buffer stride and offset;
+	stride = sizeof(VertexType);
+	offset = 0;
+
+	// Set the vertex buffer to active in the input assembler so it can be rendered.
+	deviceContext->IASetVertexBuffers(0, 1, &m_vertex_buffer, &stride, &offset);
+
+	// Set the index buffer to active in the input assembler so it can be rendered.
+	deviceContext->IASetIndexBuffer(m_indexBuffer, DXGI_FORMAT_R32_UINT, 0);
+
+	// Set the type of primitive that should be rendered from this vertex buffer, in this case triangles.
+	deviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	return;
+}
